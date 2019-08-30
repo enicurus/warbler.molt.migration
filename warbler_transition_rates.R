73d24f4976b86b14c66c3e2902c23ae26d8b56f3
Ryan Terrill <rterri2@tigers.lsu.edu>
2019-08-30 10:00:59 -0700

    Update warbler_transition_rates.R

--- warbler_transition_rates.R
+++ warbler_transition_rates.R
@@ -1,36 +1,36 @@
-#testing correlated evolution and transition rates of precense and absence of PA and SD
+`#testing correlated evolution and transition rates of precense and absence of PA and SD
 
 
 library(phytools)
+library(geiger)
 
 tree<-read.nexus("/Users/rterrill/Dropbox/Warbler.Molt.Migration/Parulidae_phylogeny/Trees/r8stree.tre")
 
 
 Data<-read.csv("~/Dropbox/Warbler.Molt.Migration/Data_treeNames.csv",row.names=1)
 
-matrix<-data.frame(Data$paextent,Data$tdExtent)
+matrix<-data.frame(Data$paextent,Data$migDist)
 
 rownames(matrix)<-rownames(Data)
 
 matrix$PA<-0
-matrix$SD<-0
+matrix$mig<-0
 
 
-
-matrix$PA<-ifelse(is.na(matrix[,1]),NA,0)
 matrix$PA<-ifelse(matrix[,1]==0,0,1)
+matrix$PA<-ifelse(is.na(matrix[,1]),0,matrix$PA)
 
 
-matrix$SD<-ifelse(is.na(matrix[,2]),NA,0)
-matrix$SD<-ifelse(matrix[,2]==0,0,1)
 
-matrix<-na.omit(matrix)
+matrix$mig<-ifelse(abs(matrix[,2])>20,1,0)
+matrix$mig<-ifelse(is.na(matrix[,2]),0,matrix$mig)
+
 
 PA<-matrix$PA
 names(PA)<-rownames(matrix)
 
-SD<-matrix$SD
-names(SD)<-rownames(matrix)
+mig<-matrix$mig
+names(mig)<-rownames(matrix)
 
 drop<-name.check(tree,PA)$tree_not_data
 
@@ -38,8 +38,20 @@ tree<-drop.tip(tree,drop)
 
 
 
-fitPagel(tree,PA,SD)
+mK<-fitPagel(tree,PA,mig)
+mK.x<-fitPagel(tree,PA,mig,drp.var="x")
+ace<-fitPagel(tree,PA,mig,method="ace")
+ace.x<-fitPagel(tree,PA,mig,method="ace",dep.var="x")
+fD<-fitPagel(tree,PA,mig,method="fitDiscrete")
+fD.x<-fitPagel(tree,PA,mig,method="fitDiscrete",dep.var="x")
 
-fitPagel(tree,PA,SD,method="ace")
 
-fitPagel(tree,PA,SD,method="fitDiscrete")
+aic<-setNames(c(mK$independent.AIC,mK$dependent.AIC,mK.x$dependent.AIC,
+	ace$independent.AIC,ace$dependent.AIC,ace.x$dependent.AIC,fD$independent.AIC,
+	fD$dependent.AIC,fD.x$dependent.AIC),c("mK independent","mK dependent","mK x dependent",
+	"ace independent", "ace dependent", "ace x dependent", "fitDiscrete independent", "fitDiscrete dependent", 
+	"fitDiscrete x dependent"))
+
+aic.w(aic)
+
+plot(ace.x,lwd.by.weight=TRUE)
